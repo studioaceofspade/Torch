b40c046f2d8e3ddac6b51badede7ba22f65ddd06
Jason Ward <jason.ward@policystat.com>
2013-02-28 21:47:19 -0500

    refs #2: deal with anon users now

--- torch/vote/models.py
+++ torch/vote/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models, IntegrityError
+from django.contrib.auth.models import User, AnonymousUser
 
 from django_extensions.db.fields import (
     CreationDateTimeField,
@@ -9,17 +9,30 @@ from torch.idea.models import Idea
 
 
 class Vote(models.Model):
-    voter = models.ForeignKey(User, related_name='votes')
+    voter = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        related_name='votes',
+    )
     idea = models.ForeignKey(Idea, related_name='votes')
+    ip = models.CharField(max_length=15, blank=True, null=True)
 
     created = CreationDateTimeField(null=True)
 
 
-def create_vote(user, idea):
-    return Vote.objects.create(
-        voter=user,
-        idea=idea,
-    )
+def create_vote(user, idea, ip=None):
+    vote_kwargs = {
+        'idea': idea,
+    }
+    if isinstance(user, AnonymousUser):
+        if ip is None:
+            raise IntegrityError('Must pass in ip address for anon users.')
+        vote_kwargs['voter'] = None
+        vote_kwargs['ip'] = ip
+    else:
+        vote_kwargs['voter'] = user
+    return Vote.objects.create(**vote_kwargs)
 
 
 def get_votes_for_idea(idea):
