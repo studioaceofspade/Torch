2909f3ebdae27b349d8b88b483df78d9246a3bb7
Jason Ward <jason.ward@policystat.com>
2013-03-08 23:12:40 -0500

    refs #11: half baked, almost ready though.

--- torch/idea/forms.py
+++ torch/idea/forms.py
@@ -1,16 +1,37 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm, ValidationError
+from django import forms
 
 from torch.idea.models import Idea
 
 
 def make_IdeaForm(user):
-    class IdeaForm(ModelForm):
+
+    class IdeaForm(forms.ModelForm):
+        title = forms.CharField(
+            label='What do you want to call your idea?',
+            required=True,
+            max_length=200,
+            widget=forms.TextInput(attrs={
+                'placeholder': 'I think it would be a good idea if Elkhart...',
+                'class': 'input-xxlarge title-input',
+                'type': 'text',
+            }),
+        )
+        description = forms.CharField(
+            required=True,
+            label='Why should people like your idea?',
+            widget=forms.Textarea(attrs={
+                'placeholder': 'The reason we should do this is...',
+                'class': 'input-xxlarge description-input',
+                'type': 'text',
+                'rows': '2',
+            }),
+        )
 
         def clean(self, *args, **kwargs):
             result = super(IdeaForm, self).clean(*args, **kwargs)
             if not isinstance(user, User):
-                raise ValidationError('This field is required.')
+                raise forms.ValidationError('This field is required.')
             return result
 
         def save(self, *args, **kwargs):
