ce2ca17da5f5418cb91b6fe296c8db5e06e8484a
Jason Ward <jason.ward@policystat.com>
2013-03-10 18:23:09 -0400

    refs #16: home page works yo

--- torch/views.py
+++ torch/views.py
@@ -1,9 +1,20 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from torch.idea.models import Idea
+
 
 def home(request):
+    ideas = Idea.objects.all().select_related()
+    most_recent_ideas = ideas.order_by(
+        '-created',
+    )[:3]
+    top_ideas = ideas.order_by(
+        '-num_votes',
+    )[:3]
     context = RequestContext(request, {
+        'most_recent_ideas': most_recent_ideas,
+        'top_ideas': top_ideas,
     })
 
     return render_to_response(
