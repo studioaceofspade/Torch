d53fd0fed92e8f2ce95eb9ce30061ef9f13f97a9
Jason Ward <jason.ward@policystat.com>
2013-03-10 16:27:30 -0400

    refs #11: got un authenticated idea creation working

--- torch/account/views.py
+++ torch/account/views.py
@@ -10,6 +10,27 @@ from django.shortcuts import redirect, render_to_response
 from torch.account.forms import UserForm
 
 
+def _deal_with_form_validation(request, form, is_create):
+    """
+    Returns a tuple of the form:
+        (True, User)
+        (False, None)
+    First section is weather or not the form was valid
+    Second section is the user object created (or logged in)
+    """
+    form_is_valid = form.is_valid()
+    user = None
+    if form_is_valid:
+        if is_create:
+            form.save()
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user.is_active:
+            django_login(request, user)
+    return form_is_valid, user
+
+
 def account(request, is_create):
     if is_create:
         Form = UserForm
@@ -18,14 +39,8 @@ def account(request, is_create):
     if request.method == 'POST':
         # Stupid AuthenticationForm
         form = Form(data=request.POST)
-        if form.is_valid():
-            if is_create:
-                form.save()
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(username=username, password=password)
-            if user.is_active:
-                django_login(request, user)
+        form_is_valid, _ = _deal_with_form_validation(request, form, is_create)
+        if form_is_valid:
             return redirect('idea_create')
     else:
         form = UserForm()
--- torch/static/js/ideaCreate.js
+++ torch/static/js/ideaCreate.js
@@ -1,30 +1,59 @@
 $(document).ready(function() {
+    $('.user-login-input input#id_first_name').hide();
+    $('.user-login-input .first-name-label').hide();
     $('.idea-create-tags a').click(function(e) {
         e.preventDefault();
         $('.idea-create-tags a').removeClass('current');
         $(this).addClass('current');
+    });
+    $('.user-login-input a').click(function(e) {
+        e.preventDefault();
+        $('.user-login-input a').removeClass('current');
+        $(this).addClass('current');
+        if ($(this).hasClass('already-have-account')) {
+            $('.user-login-input input#id_first_name').hide();
+            $('.user-login-input .first-name-label').hide();
+        } else {
+            $('.user-login-input input#id_first_name').show();
+            $('.user-login-input .first-name-label').show();
+        }
     });
     $('.submit-your-idea').click(function(e) {
         e.preventDefault();
         var title = $('.title-input input#id_title').val();
         var description = $('.description-input textarea#id_description').val();
         var tag = $('.tag-input a.current').data('val');
+        var first_name = $('.user-login-input input#id_first_name').val();
+        var username = $('.user-login-input input#id_username').val();
+        var password = $('.user-login-input input#id_password').val();
 
         var $submitForm = $('form.submit-form');
         $submitForm.find('input#id_title').val(title);
         $submitForm.find('textarea#id_description').val(description);
+        $submitForm.find('input#id_first_name').val(first_name);
+        $submitForm.find('input#id_username').val(username);
+        $submitForm.find('input#id_password').val(password);
         $submitForm.find('select#id_tag').find('option').removeAttr('selected').each(function() {
             if (parseInt($(this).val(), 10) === tag) {
                 $(this).attr('selected', 'selected');
             }
         });
+        if ($('.already-have-account').hasClass('current')) {
+            $submitForm.find('input#id_first_name').remove();
+        }
         $.ajax({
             type: 'POST',
             url: $submitForm.attr('action'),
             data: $submitForm.serialize(),
             success: function(data) {
-                $('a.thepermalink').attr('href', data.url);
-                $('.slideshow').cycle.next();
+                console.log(data);
+                if (data.url) {
+                    $('a.thepermalink').attr('href', data.url);
+                    $('.slideshow').cycle.next();
+                }
+            },
+            error: function() {
+                console.log('test');
             }
         });
     });
