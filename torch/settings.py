52d2ec2b40baea0e153fbdbb1f1f9b0c4f86af45
Jason Ward <jason.ward@policystat.com>
2013-03-15 01:11:40 -0400

    try again

--- torch/settings.py
+++ torch/settings.py
@@ -22,7 +22,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(),
 }
-ALLOWED_HOSTS = ['http://mighty-inlet-9425.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
