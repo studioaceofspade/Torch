c1edc57eeee2ac4cd06e63955a86e18695e8471e
Jason Ward <jason.ward@policystat.com>
2013-03-15 00:57:44 -0400

    fifth times the charm

--- torch/settings.py
+++ torch/settings.py
@@ -22,7 +22,6 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(),
 }
-HOST = 'localhost'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
